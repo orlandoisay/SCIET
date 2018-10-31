7f030afc295ae4aeb47bf444e7a3c0bde505beff
ThanyaLeif <32908302+ThanyaLeif@users.noreply.github.com>
2018-10-30 22:51:44 -0600

    Se agregó la clase conexión

---
+++ Data/Conexion.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Data
+{
+    public class Conexion
+    {
+        public static MySqlConnection conexion = null;
+
+        /// <summary>
+        /// Crea la conexión hacia la base de datos.
+        /// </summary>
+        /// <returns>
+        /// Retorna true si la conexión fue exitosa, de otro modo retorna false.
+        /// </returns>
+        static public bool Conectar()
+        {
+            String parametrosConexion = "Server=127.0.0.1;Database=SCIET;Uid=root;Pwd=root;Port=3306;";
+            conexion = new MySqlConnection(parametrosConexion);
+            try
+            {
+                conexion.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+       
+
+        /// <summary>
+        /// Ejecuta una sentencia en la base de datos.
+        /// </summary>
+        /// <param name="objComando"></param>
+        /// Contiene la sentencia a ingresar en la base de datos.
+        /// <param name="esInsert"></param>
+        /// <returns>
+        /// Retorna 0 si la sentencia fue ingresada correctamente, 
+        /// de otro modo retorna 1.
+        /// </returns>
+        public int ejecutarSentencia(MySqlCommand objComando, bool esInsert)
+        {
+            int resultado = 0;
+            try
+            {
+                if (Conectar())
+                {
+                    objComando.Connection = conexion;
+                    MySqlDataAdapter objAdapter = new MySqlDataAdapter(objComando);
+                    if (esInsert)
+                    {
+                        resultado = int.Parse(objComando.ExecuteScalar().ToString());
+                    }
+                    else
+                    {
+                        objComando.ExecuteNonQuery();
+                        resultado = 1;
+                    }
+                    return resultado;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+    }
+}
